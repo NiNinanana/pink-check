d35e56c65605d569f2ea10db4dcdffe5ba44e3a3
NiNinanana <yeen00123@gmail.com>
2024-02-09 15:34:06 +0900

    layout 및 header 기본 설정

---
+++ components/layout/header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="flex h-[72px] items-center justify-between px-10 shadow-md">
+      <h1 className="text-xl">MADE IN PINK</h1>
+      <div>
+        <p className="text-sm">로그인</p>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
