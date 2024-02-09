fdbf135646d3d5bad99d49fb7fc23d8d4d5b21af
NiNinanana <yeen00123@gmail.com>
2024-02-09 15:36:53 +0900

    src 폴더 생성

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="flex h-[72px] items-center justify-between px-10 shadow-md">
+    <header className="flex h-[72px] items-center justify-between bg-white px-10 shadow-md">
       <h1 className="text-xl">MADE IN PINK</h1>
       <div>
         <p className="text-sm">로그인</p>
